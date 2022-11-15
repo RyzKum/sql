bdc8de3b1d7c2c588d791a1b8ad1fbe306fe68f0
RyzKu <103863031+RyzKum@users.noreply.github.com>
2022-11-15 15:58:55 +0100

    commit

--- classe.sql
+++ classe.sql
@@ -31,53 +31,89 @@ CREATE TABLE `classes` (
   `ID` int(11) NOT NULL,
   `classe` varchar(255) NOT NULL,
   `nb_eleve` int(11) NOT NULL,
+  `id_ecole` int(11) NOT NULL,
   `id_prof` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `ecole`
---
 
 CREATE TABLE `ecole` (
   `ID` int(11) NOT NULL,
   `nom_ecole` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `matiereprof`
---
 
 CREATE TABLE `matiereprof` (
   `id_prof` int(11) NOT NULL,
   `id_matiere` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Structure de la table `matieres`
---
 
 CREATE TABLE `matieres` (
   `ID` int(11) NOT NULL,
   `nom_matiere` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `prof`
---
 
 CREATE TABLE `prof` (
-  `id` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
   `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL
+  `prenom` varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `classe` (`classe`),
+  ADD KEY `id_ecole` (`id_ecole`);
+
+
+ALTER TABLE `ecole`
+  ADD PRIMARY KEY (`ID`);
+
+
+ALTER TABLE `matiereprof`
+  ADD KEY `id_matiere` (`id_matiere`),
+  ADD KEY `id_prof` (`id_prof`);
+
+
+ALTER TABLE `matieres`
+  ADD PRIMARY KEY (`ID`);
+
+
+ALTER TABLE `prof`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_matiere` (`id_matiere`);
+
+
+ALTER TABLE `classes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `ecole`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `matieres`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `prof`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `classes`
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`id_ecole`) REFERENCES `ecole` (`ID`),
+  ADD CONSTRAINT `classes_ibfk_2` FOREIGN KEY (`id_prof`) REFERENCES `prof` (`ID`);
+
+
+ALTER TABLE `matiereprof`
+  ADD CONSTRAINT `matiereprof_ibfk_1` FOREIGN KEY (`id_prof`) REFERENCES `prof` (`ID`),
+  ADD CONSTRAINT `matiereprof_ibfk_2` FOREIGN KEY (`id_matiere`) REFERENCES `matieres` (`ID`);
+
+
+ALTER TABLE `prof`
+  ADD CONSTRAINT `prof_ibfk_1` FOREIGN KEY (`id_classe`) REFERENCES `classes` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
